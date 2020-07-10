40b0bb1202f40da1901d89cc2c74855d58b43084
hsy05 <67227190+hsy05@users.noreply.github.com>
2020-07-10 20:26:57 +0800

    Update datatype.h

--- common/datatype.h
+++ common/datatype.h
@@ -111,9 +111,11 @@ struct FootBallMsg {
 #define CHAT_WALL  0x08 //公聊
 #define CHAT_MSG   0x10 //私聊
 #define CHAT_FUNC  0x20 //功能
+#define CHAT_SYS   0x40
 
 struct ChatMsg {
     int type;
+    char name[20];
     char msg[1024];
 };
 
